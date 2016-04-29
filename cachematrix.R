d4c94b002896d715aadb19644c23e2d6701c0f6a
Elyssa Bader <elyssa-b@hotmail.com>
2016-04-29 13:32:37 -0400

    added function to retrieve cached inverse of input matrix or compute if not cached

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache an input matrix, search, and retrive the inverse of the 
+## matrix if it has been cached
+## If the inverse of the matrix has not been cached, it will be computed
 
-## Write a short comment describing this function
+## This function caches the input matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function (y){
+		x <<- y
+		m <<- NULL
+	}
+	get <- function () x
+	setinverse <- function(inverse) m <<- inverse
+	getinverse <- function () m
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function searches for the cached inverse of the input matrix and returns it if 
+## available
+## If the inverse of the matrix is not cached it will be computed
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+	if (!is.null(m)){
+	message("getting cached data")
+	return(m)
+	}
+	data <- x$get()
+	m <- solve(data,...)
+	x$setinverse(m)
+	m
 }
